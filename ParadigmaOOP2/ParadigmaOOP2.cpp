ad5d3d3a7d2ed100a18c0849283e360bf0d931fe
azmianshari <azmianshari123@gmail.com>
2023-05-29 14:48:16 +0700

    Add project files.

---
+++ ParadigmaOOP2/ParadigmaOOP2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "Orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "Orang dihapuskan\n" << endl;
+	}
+};
