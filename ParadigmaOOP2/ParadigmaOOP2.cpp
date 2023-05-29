c942d03ab643f55732af8fe89bfaebb22720f4a0
azmianshari <azmianshari123@gmail.com>
2023-05-29 15:06:24 +0700

    Membuat konstruktor pelajar

--- ParadigmaOOP2/ParadigmaOOP2.cpp
+++ ParadigmaOOP2/ParadigmaOOP2.cpp
@@ -30,4 +30,15 @@ public:
 class pelajar : private manusia, public orang {
 public:
 	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+
 };
